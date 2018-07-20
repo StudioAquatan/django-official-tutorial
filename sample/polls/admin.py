5468440385580c9e86f1a373c3c6d88cac417594
Pudding <s.kokuryo@gmail.com>
2018-07-21 01:24:38 +0900

    adminサイトでの質問の表示フィールドの順序を変更

--- sample/polls/admin.py
+++ sample/polls/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from polls.models import Question
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    """質問のAdminサイトでの表示をカスタマイズするクラス"""
+    # 表示するフィールドの順番を変更
+    fields = ['pub_date', 'question_text']
+
+
 # QuestionとChoiceをadminサイトから触れるように登録
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
