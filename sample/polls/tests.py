be672099ea30d3bf0ecc89ba51d2a1dbebb9f057
Pudding <s.kokuryo@gmail.com>
2018-07-21 00:37:03 +0900

    一覧ページのテストを追加

--- sample/polls/tests.py
+++ sample/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.shortcuts import reverse
 
 from .models import Question
 
@@ -32,3 +33,72 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text: str, days: int) -> Question:
+    """
+    Questionオブジェクトを簡単に作成するショートカット
+    :param question_text: 質問文
+    :param days: 現在の日時から前後何日か
+    :return:
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+# DBを使用するテストを書いてみる
+class QuestionIndexViewTests(TestCase):
+    """一覧表示ページのテスト"""
+
+    def test_no_questions(self):
+        """
+        表示する質問が存在しない場合のテスト
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """
+        最近投稿された質問が存在しない場合のテスト
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_question(self):
+        """
+        未来の日付の質問しか存在しない場合のテスト
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        未来の日付と過去の日付の質問の両方を含む場合のテスト
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_two_past_questions(self):
+        """
+        過去の日付の質問が複数ある場合のテスト
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
